6f5d4b45db4eddbcf424d7c998c3b85988586c3a
Christopher Wright <chrisalanwright@gmail.com>
2025-08-24 12:31:18 -0400

    finished-last-function-and-added-more-html-for-organization

--- index.js
+++ index.js
@@ -16,35 +16,61 @@ async function getParties() {
   }
 }
 
-async function getParty() {
-    try {
-        let response = await fetch(API = "/events" = id)
-        let result = await response.json()
-        chosenParty = result.data;
-        render();
-    } catch (e) {
-        console.error(e)
-    }
+async function getParty(id) {
+  try {
+    let response = await fetch(API + "/events/" + id);
+    let result = await response.json();
+    chosenParty = result.data;
+    render();
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 function PartyListItem(party) {
-  let $li = document.createElement("li")
+  let $li = document.createElement("li");
 
   if (party.id === chosenParty?.id) {
     $li.classList.add("selected");
   }
 
-  $li.innerHTML =`
+  $li.innerHTML = `
     <a href="#selected">${party.name}</a>`;
-  
-  $li.addEventListener("click",() => getParty(party.id));
-  
+
+  $li.addEventListener("click", () => getParty(party.id));
+
   return $li;
 }
 
+function PartyList() {
+  let $ul = document.createElement("ul");
+  parties.forEach((party) => {
+    $ul.appendChild(PartyListItem(party));
+  });
+  return $ul;
+}
+
+function ChosenParty() {
+  if (!chosenParty) {
+    return document
+      .createElement("p")
+      .appendChild(document.createTextNode("Select a party to see details"));
+  }
+
+  let $div = document.createElement("div");
+  $div.innerHTML = `
+    <h3>${chosenParty.name}</h3>
+    <p>Date:${chosenParty.date}</p>
+    <p>Time: ${chosenParty.time}</p>
+    <p>Location: ${chosenParty.location}</p>
+    <p>Description: ${chosenParty.description}</p>
+  `;
+  return $div;
+}
+
 function render() {
-    let $app = document.querySelector("#app");
-    $app.innerHTML = `
+  let $app = document.querySelector("#app");
+  $app.innerHTML = `
     <h1>Party Planner</h1>
     <main>
       <section>
@@ -58,11 +84,12 @@ function render() {
     </main>
     `;
 
-    $app.querySelector("PartyList").replaceWith(PartyList());
-    $app.querySelector("ChosenParty").replaceWith(chosenParty());
+  $app.querySelector("PartyList").replaceWith(PartyList());
+  $app.querySelector("ChosenParty").replaceWith(ChosenParty());
 }
 
 async function init() {
   await getParties();
   render();
 }
+init();
